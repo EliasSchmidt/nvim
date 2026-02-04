98b57f8fb60c45c142c69f9c5b8a38b057b71e04
Elias Schmidt <elias.schmidt@silbury.com>
2026-02-04 11:52:22 +0100

    add keymaps to jump visual not technical lines

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,6 @@ local sk = vim.keymap.set
 sk({"n", "v", "x"}, "<leader>w", "<cmd>w<cr>", { noremap = true, silent = true, desc = "speichern"})
 -- sk('n', '<leader>rs', ':restart<CR>', { desc = 'Neovim Neustart' })
 sk('n', '<leader>ri', function() require('custom.issue').open() end, { desc = "Öffne Config TODO.md" })
+sk('i', '<c-h>', '<c-w>')
+sk({'n', 'v', 'x'}, 'j', 'gj')
+sk({'n', 'v', 'x'}, 'k', 'gk')
